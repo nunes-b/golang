9ec91ec79868784263cbb75b694d98ed008b9a46
Rômulo Nunes <romulotg21@gmail.com>
2024-06-27 19:48:44 -0300

    [learn] variables

---
+++ modules/4/var_const.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func variables() {
+	var name string
+
+	name = "Rômulo"
+	fmt.Println(name)
+
+	var idade int
+	idade = 42
+	fmt.Println(idade)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	fruit := "apple"
+	fmt.Println(fruit)
+
+	const idadeRomulo = 10
+	fmt.Println(idadeRomulo)
+}
