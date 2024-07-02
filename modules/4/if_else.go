f541e2209fb1dae11e71509902a96cfa5eef65cb
Rômulo Nunes <romulotg21@gmail.com>
2024-07-02 07:44:05 -0300

    [learn] conditionais

---
+++ modules/4/if_else.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func if_else() {
+	valor := 13
+
+	if valor == 1 {
+		fmt.Printf("O valor é: %v\n", valor)
+	} else {
+		fmt.Printf("O valor é diferente de 1, o valor é:: %v\n", valor)
+	}
+
+	if 7%2 == 0 {
+		fmt.Println("7 e par")
+	} else {
+		resto := 7 % 2
+		fmt.Println(resto)
+		fmt.Println("7 e impar")
+	}
+}
