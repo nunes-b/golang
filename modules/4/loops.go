593493fad0681ea17950eaf026a92906ecf0ea3b
Rômulo Nunes <romulotg21@gmail.com>
2024-07-03 11:33:56 -0300

    for_loops

---
+++ modules/4/loops.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Loops
+// Laçõs de repetições
+
+func main() {
+	// sum := 0
+
+	// * for *
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println("index: ", i)
+	// 	sum += i
+	// 	fmt.Println("soma: ", sum)
+	// }
+
+	// * for infinito *
+	// for {
+	// 	fmt.Println("loop infinito")
+	// 	time.Sleep(2 * time.Second)
+	// }
+
+	// * for range *
+	frutas := []string{"maca", "banana", "laranja"}
+	for i, fruta := range frutas {
+		fmt.Println(i, fruta)
+	}
+
+}
